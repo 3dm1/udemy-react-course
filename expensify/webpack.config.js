d6f51b2d6773e5f1db5e0dffb75ed9e040ba8ccf
Edson Menegatti <edson.menegatti@gmail.com>
2019-02-16 11:56:33 -0200

    Extract source maps and styles files from bundle.js

--- expensify/webpack.config.js
+++ expensify/webpack.config.js
@@ -1,29 +1,50 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  mode: 'development',
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ],
-      test: /\.s?css$/,
-    }]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  },
-  devtool:'cheap-module-eval-source-map'
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({
+    filename: 'styles.css'
+  });
+
+  return {
+    plugins: [
+      CSSExtract
+    ],
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map'
+  }
 };
